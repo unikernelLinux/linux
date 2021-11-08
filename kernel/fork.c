3c42a7088fda6206e4558b5f89712dc2eb079949
Ali Raza <aliraza@bu.edu>
2021-11-08 17:05:33 -0500

    This seems to be necessary to boot UKL baremetal

--- kernel/fork.c
+++ kernel/fork.c
@@ -1184,6 +1184,8 @@ struct file *get_mm_exe_file(struct mm_struct *mm)
 	exe_file = rcu_dereference(mm->exe_file);
 	if (exe_file && !get_file_rcu(exe_file))
 		exe_file = NULL;
+	if (get_in_user() > 0)
+		exe_file = NULL;
 	rcu_read_unlock();
 	return exe_file;
 }
