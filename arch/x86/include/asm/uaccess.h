d40561218d0cf6264de3a53ae775806930d0d453
Ali Raza <aliraza@bu.edu>
2022-06-23 10:43:29 -0400

    x86/uaccess: Make access_ok UKL aware
    When configured for UKL, access_ok needs to account for the unified address
    space that is used by the kernel and the process being run. To do this,
    they need to check the task struct field added earlier to determine where
    the execution that is making the check is running. For a zero value, the
    normal boundary definitions apply, but non-zero value indicates a UKL
    thread and a shared address space should be assumed.
    
    Signed-off-by: Ali Raza <aliraza@bu.edu>

--- arch/x86/include/asm/uaccess.h
+++ arch/x86/include/asm/uaccess.h
@@ -37,11 +37,19 @@ static inline bool pagefault_disabled(void);
  * Return: true (nonzero) if the memory block may be valid, false (zero)
  * if it is definitely invalid.
  */
+#ifdef CONFIG_UNIKERNEL_LINUX
+#define access_ok(addr, size)					\
+({									\
+	WARN_ON_IN_IRQ();						\
+	(is_ukl_thread() ? 1 : likely(__access_ok(addr, size)));	\
+})
+#else
 #define access_ok(addr, size)					\
 ({									\
 	WARN_ON_IN_IRQ();						\
 	likely(__access_ok(addr, size));				\
 })
+#endif
 
 #include <asm-generic/access_ok.h>
 
