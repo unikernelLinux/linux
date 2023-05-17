84b8d899628f86a163010224b1d801d74d7f4716
Eric B Munson <munsoner@bu.edu>
2023-05-17 13:12:52 -0400

    Add UKL Shortcutting code behind config option
    We want these to be available, but they are unlikley to be sent
    upstream. We will control usage with a config option that will enable
    them only when requested.
    
    Signed-off-by: Eric B Munson <munsoner@bu.edu>

---
+++ net/ipv4/ukl_shortcuts.c
@@ -0,0 +1,86 @@
+#include <linux/types.h>
+#include <linux/file.h>
+#include <linux/fs.h>
+#include <linux/skbuff.h>
+#include <linux/scatterlist.h>
+#include <linux/socket.h>
+#include <linux/uio.h>
+#include <linux/err.h>
+
+#include <net/inet_common.h>
+#include <net/tcp.h>
+#include <net/sock.h>
+
+int shortcut_tcp_sendmsg(int fd, void *data, size_t len)
+{
+	struct iovec iov;
+	struct msghdr msg;
+	struct fd f;
+	struct socket *socket;
+	struct sock *sk;
+	struct kiocb kiocb;
+	loff_t pos, *ppos;
+	int ret = 0;
+
+	iov.iov_base = data;
+	iov.iov_len = len;
+
+	iov_iter_init(&msg.iter, ITER_SOURCE, &iov, 1, iov.iov_len);
+
+	f = fdget_pos(fd);
+	ppos = file_ppos(f.file);
+	if (ppos) {
+		pos = *ppos;
+		ppos = &pos;
+	}
+
+	socket = (struct socket *)f.file->private_data;
+	sk = socket->sk;
+
+	init_sync_kiocb(&kiocb, f.file);
+	kiocb.ki_pos = (ppos ? *ppos : 0);
+	msg.msg_iocb = &kiocb;
+
+	/* Up until now, this has all been setup, lock the socket and send */
+	lock_sock(sk);
+	ret = tcp_sendmsg_locked(sk, &msg, iov.iov_len);
+	release_sock(sk);
+
+	return ret;
+}
+EXPORT_SYMBOL(shortcut_tcp_sendmsg);
+
+int shortcut_tcp_recvmsg(int fd, void *buf, size_t len)
+{
+	struct iovec iov;
+	struct msghdr msg;
+	struct fd f;
+	struct sock *sk;
+	struct kiocb kiocb;
+	loff_t pos, *ppos;
+	int addr_len;
+	int ret = 0;
+
+	iov.iov_base = data;
+	iov.iov_len = len;
+
+	iov_iter_init(&msg.iter, ITER_DEST, &iov, 1, iov.iov_len);
+
+	f = fdget_pos(fd);
+	ppos = file_ppos(f.file);
+	if (ppos) {
+		pos = *ppos;
+		ppos = &pos;
+	}
+
+	sk = ((struct socket *)f.file->private_data)->sk;
+
+	init_sync_kiocb(&kiocb, f.file);
+	kiocb.ki_pos = (ppos ? *ppos : 0);
+	msg.msg_iocb = &kiocb;
+
+	ret = tcp_recvmsg(sk, &msg, len, 0, &addr_len);
+
+	return ret;
+}
+EXPORT_SYMBOL(shortcut_tcp_recvmsg)
