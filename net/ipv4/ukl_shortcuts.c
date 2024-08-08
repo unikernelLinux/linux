88810cd1b80735e35e17a561f36c470ab5412d64
Eric B Munson <munsoner@bu.edu>
2024-08-08 21:00:31 +0000

    Fix some silly typos and the usage of the file position marker
    Given that these are always streams (we know they are TCP connections)
    there is no need to calculate the stream position, it is always 0.
    
    Fix the creation of the msghdr structure.
    
    Signed-off-by: Eric B Munson <munsoner@bu.edu>

--- net/ipv4/ukl_shortcuts.c
+++ net/ipv4/ukl_shortcuts.c
@@ -14,29 +14,24 @@
 int shortcut_tcp_sendmsg(int fd, struct iovec *iov)
 {
 	struct fd f;
-	struct socket *socket;
 	struct sock *sk;
 	struct kiocb kiocb;
-	loff_t pos, *ppos;
+	struct msghdr msg;
 	int ret = 0;
 
 	f = fdget_pos(fd);
-	ppos = file_ppos(f.file);
-	if (ppos) {
-		pos = *ppos;
-		ppos = &pos;
-	}
 
-	socket = (struct socket *)f.file->private_data;
-	sk = socket->sk;
+	sk = ((struct socket *)f.file->private_data)->sk;
+
+	iov_iter_init(&msg.msg_iter, ITER_SOURCE, iov, 1, iov->iov_len);
 
 	init_sync_kiocb(&kiocb, f.file);
-	kiocb.ki_pos = (ppos ? *ppos : 0);
+	kiocb.ki_pos = 0;
 	msg.msg_iocb = &kiocb;
 
 	/* Up until now, this has all been setup, lock the socket and send */
 	lock_sock(sk);
-	ret = tcp_sendmsg_locked(sk, &msg, iov.iov_len);
+	ret = tcp_sendmsg_locked(sk, &msg, iov->iov_len);
 	release_sock(sk);
 
 	return ret;
@@ -46,12 +41,9 @@ EXPORT_SYMBOL(shortcut_tcp_sendmsg);
 int shortcut_tcp_write(int fd, void *data, size_t len)
 {
 	struct iovec iov;
-	struct msghdr msg;
 
 	iov.iov_base = data;
 	iov.iov_len = len;
-
-	iov_iter_init(&msg.iter, ITER_SOURCE, &iov, 1, iov.iov_len);
 
 	return shortcut_tcp_sendmsg(fd, &iov);
 }
@@ -64,26 +56,20 @@ int shortcut_tcp_recvmsg(int fd, void *buf, size_t len)
 	struct fd f;
 	struct sock *sk;
 	struct kiocb kiocb;
-	loff_t pos, *ppos;
 	int addr_len;
 	int ret = 0;
 
-	iov.iov_base = data;
+	iov.iov_base = buf;
 	iov.iov_len = len;
 
-	iov_iter_init(&msg.iter, ITER_DEST, &iov, 1, iov.iov_len);
+	iov_iter_init(&msg.msg_iter, ITER_DEST, &iov, 1, iov.iov_len);
 
 	f = fdget_pos(fd);
-	ppos = file_ppos(f.file);
-	if (ppos) {
-		pos = *ppos;
-		ppos = &pos;
-	}
 
 	sk = ((struct socket *)f.file->private_data)->sk;
 
 	init_sync_kiocb(&kiocb, f.file);
-	kiocb.ki_pos = (ppos ? *ppos : 0);
+	kiocb.ki_pos = 0;
 	msg.msg_iocb = &kiocb;
 
 	ret = tcp_recvmsg(sk, &msg, len, 0, &addr_len);
